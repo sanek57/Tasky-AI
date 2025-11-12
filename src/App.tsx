e20597721c70efba04712998cdf5f473b9dd55c3
sanek57 <sanekofblack@gmail.com>
2025-11-12 11:57:46 +1000

    init project

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+
+function App() {
+
+  return <div className="text-4xl">App</div>
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+// node modules
+import { createRoot } from 'react-dom/client'
+
+// CSS link
+import './index.css'
+
+// Components
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(<App />)
